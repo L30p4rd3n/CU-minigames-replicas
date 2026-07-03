1fbfcd26d04d7bb83bf7cbc3ebfe3273910f74ea
L30p4rd3n <pzkpfwiilu4@gmail.com>
2026-07-03 08:06:33 +0300

    Change image loading, add background image

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import {Mouse} from "./mouse";
 const canvas = document.getElementById("canvas-main")! as HTMLCanvasElement;
 const HEIGHT = 400;
 const WIDTH = 400;
-const SCALE = 1;
+const SCALE = 2;
 
 const updateRate = 60;
 const DeltaTime = 1/60; // s
@@ -19,8 +19,9 @@ const loadImage = (path: string): Promise<HTMLImageElement> => {
     image.src = path;
     return new Promise(r => {image.onload = () => r(image)});
 };
+const keypadImage = await loadImage("../assets/keypad.png");
 
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d")!;
 const mouse: Mouse = {x: 0, y: 0, captured_output: 0, clicked: false};
 const keypad = new Ctx();
 
@@ -40,17 +41,21 @@ function mouseUp(e: MouseEvent, mouse: Mouse): void{
 canvas.addEventListener("mousemove", (e: MouseEvent) =>{
     mouse.x = e.clientX;
     mouse.y = e.clientY;
+    console.log(mouse.x, mouse.y);
 });
 
 canvas.addEventListener("mousedown", () => {
     keypad.check_click(mouse);
+    console.log("mouse down");
 });
 
 canvas.addEventListener("mouseup", () => {
     mouse.clicked = false;
     mouse.captured_output = -3;
+    console.log("mouse up");
 })
 
-const drawBase = () => {
-    
+const draw_base = () => {
+    ctx.drawImage(keypadImage, 0, 0);
+
 }
